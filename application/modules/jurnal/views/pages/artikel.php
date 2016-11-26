3870029eb62992f810197fd5ae0a1bbde9aa2b78
Bassamtiano Renaufalgi <bassamtiano@gmail.com>
2016-11-26 13:27:33 +0900

    Add share facebook
    Share facebok

--- application/modules/jurnal/views/pages/artikel.php
+++ application/modules/jurnal/views/pages/artikel.php
@@ -14,6 +14,9 @@
 	<div class="row jurnal-wrapper">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="jw-item">
+
+				<?php $date = date_parse($data->created_at); ?>
+
 				<div class="jwi-category" >
 					<span class="category-title ">
 						<?php echo $data->kategori ?>
@@ -39,6 +42,12 @@
 					<?php
 						echo $data->content;
 					?>
+
+					<div style="width: 100%; float: left; padding: 10px;">
+						<a href="http://www.facebook.com/share.php?u=<?php echo base_url('index.php/jurnal/') . str_replace('_', ' ', strtolower($data->kategori)) . '/' . $date['year'] . '/' . $date['month'] . '/' . $date['day'] . '/' . $data->id; ?>&title=[<?php echo $data->title; ?>]" onclick="window.open(this.href, 'newwindow', 'width=300, height=250'); return false;">
+							<i class="fa fa-facebook" style="margin-right: 10px;"></i>Share on Facebook
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
